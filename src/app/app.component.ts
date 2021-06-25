73f28448193844306dc2d58b1fd5f6d9fba0aafa
Niki Micallef <nikmic94@gmail.com>
2021-06-25 20:05:36 +0200

    Added upvote/downvote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,11 @@ export class AppComponent implements OnInit {
   topContent: Content[] = []
   displayedContent: Content[] = []
   showAllContent = true;
-  gettingMoreContent = false;
+  gettingContent = false;
   activeContent: Content = { id: "", mediaType: "IMAGE", source: "", title: "", contentUrl: "", previewUrl: "", votes: 0, description: "", length: 0, aspectRatio: "", topic: "" };
   activeContentListIndex: number = 0;
+  sendingVote = false;
+  justVoted = false;
   private contentItems = 15;
   private pageNumber = 0;
   private url = 'http://localhost:8080/content'
@@ -30,16 +32,49 @@ export class AppComponent implements OnInit {
       .subscribe(content => { this.allContent = content; this.displayedContent = this.allContent });
   }
 
-  switchToAllContent() {
-    this.showAllContent = true;
-    this.displayedContent = this.allContent;
-  }
+  // HTTP Requests
 
   getContent(contentItems: number, pageNumber: number): Observable<Content[]> {
     return this.http.get<Content[]>(this.url + "?page=" + pageNumber + "&pageSize=" + contentItems)
       .pipe(
-        catchError(this.handleError<Content[]>('getContent', []))
+        catchError(this.handleError<Content[]>('GET All Content'))
       );
+  }
+
+  getTopContent(): Observable<Content[]> {
+    return this.http.get<Content[]>(this.url + "?orderByVotesDesc=true&pageSize=10")
+      .pipe(
+        catchError(this.handleError<Content[]>('GET Top Content'))
+      );
+  }
+
+  upvoteContent(contentId : string): Observable<Content> {
+    return this.http.post<Content>(this.url + "/" + contentId + "/actions/upvote", {})
+      .pipe(
+        catchError(this.handleError<Content>('Upvote Content'))
+      );
+  }
+
+  downvoteContent(contentId : string): Observable<Content> {
+    return this.http.post<Content>(this.url + "/" + contentId + "/actions/downvote", {})
+      .pipe(
+        catchError(this.handleError<Content>('Downvote Content'))
+      );
+  }
+
+  private handleError<T>(operation: string) {
+    return (error: any): Observable<T> => {
+
+      console.error("Error when executing " + operation); 
+
+      // Let the app keep running by returning an empty result.
+      return of({} as T);
+    };
+  }
+
+  switchToAllContent() {
+    this.showAllContent = true;
+    this.displayedContent = this.allContent;
   }
 
   switchToTopContent() {
@@ -58,18 +93,11 @@ export class AppComponent implements OnInit {
     }
   }
 
-  getTopContent(): Observable<Content[]> {
-    return this.http.get<Content[]>(this.url + "?orderByVotesDesc=true&pageSize=10")
-      .pipe(
-        catchError(this.handleError<Content[]>('getContent', []))
-      );
-  }
-
   loadMoreContent() {
-    this.gettingMoreContent = true;
+    this.gettingContent = true;
     this.pageNumber += 1;
     this.getContent(this.contentItems, this.pageNumber)
-      .subscribe(content => { Array.prototype.push.apply(this.allContent, content); this.displayedContent = this.allContent; this.gettingMoreContent = false; });
+      .subscribe(content => { Array.prototype.push.apply(this.allContent, content); this.displayedContent = this.allContent; this.gettingContent = false; });
   }
 
   onModalClose() {
@@ -78,7 +106,7 @@ export class AppComponent implements OnInit {
 
   incrementActiveImage() {
     if (this.activeContentListIndex === this.displayedContent.length - 1) {
-      this.gettingMoreContent = true;
+      this.gettingContent = true;
       this.pageNumber += 1;
       this.getContent(this.contentItems, this.pageNumber)
         .subscribe(content => {
@@ -86,11 +114,13 @@ export class AppComponent implements OnInit {
           this.displayedContent = this.allContent;
           this.activeContentListIndex += 1;
           this.activeContent = this.displayedContent[this.activeContentListIndex];
-          this.gettingMoreContent = false;
+          this.gettingContent = false;
+          this.justVoted = false;
         });
     } else {
       this.activeContentListIndex += 1;
       this.activeContent = this.displayedContent[this.activeContentListIndex];
+      this.justVoted = false;
     }
   }
 
@@ -98,6 +128,7 @@ export class AppComponent implements OnInit {
     if(this.activeContentListIndex > 0) {
       this.activeContentListIndex -= 1;
       this.activeContent = this.displayedContent[this.activeContentListIndex];
+      this.justVoted = false;
     }
   }
 
@@ -106,19 +137,33 @@ export class AppComponent implements OnInit {
     this.activeContentListIndex = index;
   }
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      console.error(error); // log to console instead
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+  vote(content : Content, upvote: boolean) {
+    this.sendingVote = true;
+    if(upvote) {
+      this.upvoteContent(content.id)
+        .subscribe(newContent => {
+          this.updateContentAfterVote(newContent);
+          this.sendingVote = false;
+          this.justVoted = true;
+        })
+    } else {
+      this.downvoteContent(content.id)
+        .subscribe(newContent => {
+          this.updateContentAfterVote(newContent);
+          this.sendingVote = false;
+          this.justVoted = true;
+        })
+    }
   }
+
+  private updateContentAfterVote(newContent: Content) {
+    this.activeContent = newContent;
+    this.displayedContent[this.activeContentListIndex] = newContent;
+    if(this.showAllContent) {
+      this.allContent[this.activeContentListIndex] = newContent;
+    } else {
+      this.topContent[this.activeContentListIndex] = newContent;
+    }
+  }
+
 }
